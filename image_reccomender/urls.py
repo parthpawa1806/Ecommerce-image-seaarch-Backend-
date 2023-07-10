0213da13c326529af0f98b04446018d4738bd7eb
parthpawa1806 <parthpawa1806@gmail.com>
2023-07-10 23:51:17 +0530

    finalllll

--- image_reccomender/urls.py
+++ image_reccomender/urls.py
@@ -8,3 +8,5 @@ urlpatterns = [
     path('api/', include('app_name.urls')),
 ]
 
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
