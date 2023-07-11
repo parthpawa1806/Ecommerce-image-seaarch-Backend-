1b7d34abff4f7487c49cc0e3efb6548382c676e1
parthpawa1806 <parthpawa1806@gmail.com>
2023-07-11 15:37:34 +0530

    fianladwebb

--- image_reccomender/settings.py
+++ image_reccomender/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_*vpsa*v%*ruvp$ev*$)v9w1!%d!17(c(lgfc54jw5$^59!x&1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = ['127.0.0.1','.vercel.app', '.now.sh']
 
 # Application definition
 
